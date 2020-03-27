53bf4b57af6f423f136e4b0437fefbc78899c8c1
FernandoHurtado <mrfe333@gmail.com>
2020-03-27 07:34:06 -0600

    struct inicial

---
+++ Mochila/mochila.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+class Elementos {
+
+
+
+	Elementos () {
+	
+	}
+
+	CalcularBeneficio () {
+	
+	}
+
+}
+
+class Mochila {
+
+	Mochila () {
+	
+	// crear vector
+	}
+
+	LlenarMochila () {
+	
+	}
+
+	ImprimirMochila () {
+	
+	
+	}
+
+}
+
+
+int main {
+
+	
+
+}

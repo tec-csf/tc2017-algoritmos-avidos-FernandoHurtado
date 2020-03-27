6d0c8cd26e4c0350b575eac837cf8f806baa1beb
FernandoHurtado <mrfe333@gmail.com>
2020-03-27 13:03:39 -0600

    upd

--- Mochila/mochila.cpp
+++ Mochila/mochila.cpp
@@ -3,27 +3,37 @@
 
 using namespace std;
 
+struct Elemento {
+	float peso;
+	int beneficio;
+};
 
-
-class Elementos {
-
-
-
-	Elementos () {
-	
-	}
-
-	CalcularBeneficio () {
-	
-	}
-
-}
 
 class Mochila {
 
-	Mochila () {
+	public:
 	
-	// crear vector
+	int capacidad;
+	int num_elem = 10;
+	float PesoBeneficio[num_elem];
+
+	Mochila (int capacidad) {
+		this -> capacidad = capacidad;
+		
+		elemento elementos[num_elem];
+		int solucion[num_elem];
+
+		for (int i=0; i<num_elem; i++){
+			elementos[i].peso = (rand()%10)+1;
+			elementos[i].beneficio = (rand()%15)+1;
+			PesoBeneficio[i] = elementos[i].peso/elementos[i].beneficio;
+		}
+
+		for (int i = 0; i < num_elem -1; i++){
+		
+		
+		}
+		
 	}
 
 	LlenarMochila () {
@@ -35,11 +45,12 @@ class Mochila {
 	
 	}
 
-}
-
+};
 
 int main {
 
 	
 
-}
+
+	return 0;
+};
